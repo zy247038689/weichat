d199bf273b615eed549c4850a96150d7cada69e1
zy247038689 <zy247038689@gmail.com>
2020-05-11 14:58:35 +0900

    first commit

---
+++ app/src/main/java/com/example/weixin/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.weixin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.os.Bundle;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String[] VIDEO_PERMISSIONS = {Manifest.permission.CAMERA,Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private static final int VIDEO_PERMISSIONS_CODE = 1;
+    private TextView et_user_name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et_user_name = findViewById(R.id.et_user_name);
+        Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+        startActivityForResult(intent,1);
+        requestPermission();
+    }
+
+    //申请权限
+    private void requestPermission() {
+        // 当API大于 23 时，才动态申请权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(MainActivity.this,VIDEO_PERMISSIONS,VIDEO_PERMISSIONS_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case VIDEO_PERMISSIONS_CODE:
+                //权限请求失败
+                if (grantResults.length == VIDEO_PERMISSIONS.length) {
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            //弹出对话框引导用户去设置
+                            showDialog();
+                            Toast.makeText(MainActivity.this, "请求权限被拒绝", Toast.LENGTH_LONG).show();
+                            break;
+                        }
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this, "已授权", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+
+    //弹出提示框
+    private void showDialog(){
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setMessage("微信需要相机、录音和读写权限，是否去设置？")
+                .setPositiveButton("是", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        goToAppSetting();
+                    }
+                })
+                .setNegativeButton("否", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+
+    // 跳转到当前应用的设置界面
+    private void goToAppSetting(){
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data!=null){
+            String userName = data.getStringExtra("userName");
+            //if (!TextUtils.isEmpty(userName)){
+            Toast.makeText(MainActivity.this,"登陆成功："+userName, Toast.LENGTH_SHORT).show();
+            et_user_name.setText(userName);
+            //}
+        }
+    }
+}
+
+
